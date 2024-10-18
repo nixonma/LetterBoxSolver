6f1320ca9e383055deba82efbf78fea16e2dfb0d
nixonma <nixonm@rose-hulman.edu>
2024-10-18 15:35:05 -0700

    added pseudo code for the process

--- LttrBxSolver/src/Main.java
+++ LttrBxSolver/src/Main.java
@@ -3,16 +3,28 @@ import javax.swing.*;
 public class Main {
     public static void main(String args[]){
         String result = JOptionPane.showInputDialog("input the 12 letters clockwise from top left, space separated");
-        String[] input = new String[12];
+        String[] letters = new String[12];
         try{
-            input = result.split(" ");
+            letters = result.split(" ");
         } catch(Error error){
             System.err.println("Yo dawg, we read the input rules?");
             System.exit(0);
         }
-        for(String a : input){
+
+        result = JOptionPane.showInputDialog("enter the number of words the puzzle suggests to solve within");
+        int lengthRec = Integer.parseInt(result);
+        for(String a : letters){
             System.out.println(a);
         }
+
+        //create a representation of the board for this puzzle
         Board b = new Board();
+
+        //read in a list of possible words to use
+        //clean unusable words from list
+
+        //sort words into lists based on starting letter
+
+        //try and create lists of words less than recommended length
     }
 }
