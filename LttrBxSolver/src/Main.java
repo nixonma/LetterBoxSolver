59a2b66e24fff5d8daa63aa1872a9fa6a6c97c30
nixonma <nixonm@rose-hulman.edu>
2024-10-23 11:18:42 -0700

    cleaned out a few more words from the text file and altered the solve implementation to improve runtime.

--- LttrBxSolver/src/Main.java
+++ LttrBxSolver/src/Main.java
@@ -1,18 +1,17 @@
 import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     private static int lengthRec;
     private static Board board;
     private static Set<String> validWordSet;
+    private static Map<Character, Set<String>> startsWithMap;
     private static int num3WordSolutions = 0;
     private static int num2WordSolutions = 0;
     private static int num4WordSolutions = 0;
+    private static long loopItt = 0;
     public static void main(String args[]){
         String result = JOptionPane.showInputDialog("input the 12 letters clockwise from top left in sets of 3, space separated. \n (ex: abc def ghi jkl)");
         String[] sides = new String[4];
@@ -48,18 +47,33 @@ public class Main {
         }
         System.out.println("Starting with " + wordSet.size() + " words");
         //clean unusable words from list
-        validWordSet = new HashSet<String>();
+        validWordSet = new HashSet<>();
+        startsWithMap = new HashMap<>();
         for(String word : wordSet){
-            if(board.isValidWord(word))
+            if(board.isValidWord(word)) {
                 validWordSet.add(word);
+                Character c = word.charAt(0);
+                if(startsWithMap.containsKey(c)){
+                    Set<String> charSet = startsWithMap.get(c);
+                    charSet.add(word);
+                } else {
+                    HashSet<String> temp = new HashSet<>();
+                    temp.add(word);
+                    startsWithMap.put(c, temp);
+                }
+            }
         }
         System.out.println("Condensed to " + validWordSet.size() + " words");
+        for(Character c : startsWithMap.keySet()){
+            System.out.println(startsWithMap.get(c).size() + " words beginning with " + c);
+        }
 
         ArrayList<String> solution = new ArrayList<>();
         solveSetOrder("", solution);
-        System.out.println("found " + num2WordSolutions + " solutions");
-        System.out.println("found " + num3WordSolutions + " solutions");
-        System.out.println("found " + num4WordSolutions + " solutions");
+        System.out.println("looped " + loopItt + " times");
+        System.out.println("found " + num2WordSolutions + " 2-word solutions");
+        System.out.println("found " + num3WordSolutions + " 3-word solutions");
+        System.out.println("found " + num4WordSolutions + " 4-word solutions");
     }
 
     /*
@@ -74,18 +88,29 @@ public class Main {
             }else {
                 num4WordSolutions++;
             }
-            System.out.println("Solution: " + solution.toString() + " Yay! ");
+//            System.out.println("Solution: " + solution.toString() + " Yay! ");
             return;
         }
         if(solution.size() > lengthRec - 1){
             return;
         }
-        for(String s : validWordSet){
-            if(current.equals("") || current.charAt(current.length() - 1) == s.charAt(0)){
+        if(current.length() > 0){
+            Character c = current.charAt(current.length() - 1);
+            for(String s : startsWithMap.get(c)){
+                loopItt++;
                 solution.add(s);
                 solveSetOrder(s, solution);
                 solution.remove(solution.size() - 1);
             }
+            return;
+        }
+        for(String s : validWordSet){
+            loopItt++;
+//            if(current.equals("") || current.charAt(current.length() - 1) == s.charAt(0)){
+                solution.add(s);
+                solveSetOrder(s, solution);
+                solution.remove(solution.size() - 1);
+//            }
         }
     }
 }
